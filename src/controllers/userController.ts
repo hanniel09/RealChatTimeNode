a89f993d19397c2ba7c30f52e914828cd689ff2b
hanniel09 <hannielvieira1227@gmail.com>
2023-04-20 22:04:03 -0300

    feature

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,9 +3,11 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import User from "../models/user"
 import UserService from '../services/UserService';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { ParsedQs } from 'qs';
 
 
 class UserController extends UserService {
-  
-}
+  }
+
 export default UserController;
